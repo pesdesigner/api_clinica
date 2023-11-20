547c2d86b7e5833fd3877c1b38cda6e5f8759c70
Paulo Eduardo <pauloeduardo.dg@outlook.com>
2023-11-20 17:17:30 -0300

    Classe Paciente, métodos Salvar, Excluir, Deletar e Listar

---
+++ src/main/java/com/graficoeweb/api/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,12 @@
+package com.graficoeweb.api.paciente;
+
+import com.graficoeweb.api.endereco.DadosEndereco;
+import jakarta.validation.Valid;
+
+public record DadosAtualizacaoPaciente(
+        Long id,
+        String nome,
+        String telefone,
+        @Valid DadosEndereco endereco
+) {
+}
